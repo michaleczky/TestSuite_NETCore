40de04bdb68c3617214392aef66e35db6bd76154
Peter Michaleczky <peter@Macs-MacBook-Air.local>
2016-09-16 11:08:54 +0200

    Google Oauth2 authentication added

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # TestSuite_NETCore
-Test Suite in .NET Core
+Test Suite in .NET Core. Features:
+- Google OAuth 2 integration
+
+# Prerequisites and installation (OS X)
+
+1. [.NET Core runtime](https://www.microsoft.com/net/core#macos)
+2. Run `dotnet restore` to install project dependencies.
+3. Create database and run migrations with `dotnet ef database update` 
+
+# Run the app
+
+1. Start the app with `dotnet run`.
+2. Open the `http://localhost:5000` in browser.
+
+# Troublshooting
+
+## Google OAuth2 authentication
+
+### Arriving to a blank page after succesfull login
+
+Try to empty the browser cache or open the app in a private browser window (to avoid previous sessions remain cached in the browser). 
+
---
+++ Startup.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using PxTestSuite.Data;
+using PxTestSuite.Models;
+using PxTestSuite.Services;
+
+namespace PxTestSuite
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see https://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets();
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddMvc();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, AuthMessageSender>();
+            services.AddTransient<ISmsSender, AuthMessageSender>();
+
+            // Add authentication middleware and inform .NET Core MVC what scheme we'll be using
+            // services.AddAuthentication(options => options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme);
+    
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseIdentity();
+
+            // Add external authentication middleware below. To configure them please see https://go.microsoft.com/fwlink/?LinkID=532715
+
+            // Adds a cookie-based authentication middleware to application
+            // app.UseCookieAuthentication(new CookieAuthenticationOptions
+            // {
+            //     LoginPath = "/account/login",
+            //     AuthenticationScheme = "Cookies",
+            //     AutomaticAuthenticate = true,
+            //     AutomaticChallenge = true
+            // });
+
+            // Google authentication
+            app.UseGoogleAuthentication(new GoogleOptions()
+                {
+                    ClientId = "10349815067-30ih74pflogqjdem01di9k9rhl2t6vuj.apps.googleusercontent.com",
+                    ClientSecret = "L-WnQSe4tK0k8_VHgfi8g7sN",
+                    Scope = { "email", "openid" }
+                });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
